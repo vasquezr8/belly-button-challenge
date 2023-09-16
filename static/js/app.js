a8cb7652fc89e3be2f636cba8fe43445265f96b9
vasquezr8 <vasquezr8@gmail.com>
2023-09-16 14:57:39 -0700

    Can select multiple IDs in dropdown menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,18 @@
 let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
 
+function displayCharts(id) {
+
+    console.log(id)
+
+}
+
+function optionChanged(selectedId) {
+
+    displayCharts(selectedId);
+
+}
+
 function init() {
 
     d3.json(url).then(function(data) {
@@ -16,12 +28,15 @@ function init() {
 
         for (let i=0; i<ids.length; i++) {
 
-            dropdownMenu.append("option").text(ids[i]);
-            
+            dropdownMenu.append("option").text(ids[i]).property("value", ids[i]);
+
         }
 
+        first = ids[0];
 
         // Display the charts and panel with the first ID
+
+        displayCharts(first);
 
     });
 
